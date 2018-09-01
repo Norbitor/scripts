2a4e0d35eb96e0c1d9a7c34c7417d48d7c8979db
Norbert Langner <norbitor@gmail.com>
2018-09-01 23:48:18 +0200

    Checks if create db and unix user, fixed some commands

--- nginx-helpers/nginx_create_vhost.sh
+++ nginx-helpers/nginx_create_vhost.sh
@@ -8,17 +8,18 @@ readonly WWW_DIR="/var/www"
 # FUNCTIONS
 function path_check()
 {
-    if [ ! -d $TEMPLATE_DIR ] && [ ! -d $VHOST_DIR ] && [ ! -d $WWW_DIR ]; then
-        echo "Configured paths are invalid. Check them or fix the CONSTANTS at the top"
-        exit 3
-    fi    
+    echo "mocked path check"
+#    if [ ! -d $TEMPLATE_DIR ] && [ ! -d $VHOST_DIR ] && [ ! -d $WWW_DIR ]; then
+#        echo "Configured paths are invalid. Check them or fix the CONSTANTS at the top"
+#        exit 3
+#    fi    
 }
 
 function create_project_directory()
 {
-    echo "mkdir $WWW_DIR/$1"
-    echo "mkdir $WWW_DIR/$1/public_html"
+    echo "mkdir -p $WWW_DIR/$1/public_html"
     echo "chown -R :www-data $WWW_DIR/$1"
+    echo "chmod g+ws -R $WWW_DIR/$1"
 }
 
 function copy_template()
@@ -29,7 +30,21 @@ function copy_template()
 
 function configure_database()
 {
-    echo "mysql -u root"
+    if [ $1 != "yes" ]; then
+        echo "You decided to NOT create a database"
+    else
+        echo "mysql -u root ..."
+    fi
+}
+
+function create_user()
+{
+    if [ $1 != "yes" ]; then
+        echo "You decided to NOT create a user"
+    else
+        echo "adduser --home $WWW_DIR/$2 user_foo" #TODO: some regex to change eg. somesite.com to somesite
+        echo "echo \"umask 002 >> $WWW_DIR/$2/.profile\""
+    fi
 }
 
 # SCRIPT MAIN
@@ -40,18 +55,19 @@ if [ `whoami` != "root" ]; then
     exit 2
 fi
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 site_domain template create_db"
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 site_domain template create_user create_db"
     echo "    site_domain: full-qualified domain name (FQDN) without www"
     echo "    template: vhost template to use"
+    echo "    create_user: [yes|no] should script create Linux user?"
     echo "    create_db: [yes|no] should script create MySQL DB?"
     exit 1
 fi
 
-#path_check
+path_check
+create_user $3 $1
 create_project_directory $1
 copy_template $2 $1
-configure_database $3
+configure_database $4
 
 echo "All done! Please verify $VHOST_DIR/$1 configuration file before enabling it."
-
